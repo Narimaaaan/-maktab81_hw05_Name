ec3982d3ca92a9c948f0fe0669f58c0aff97bf71
Nariman Najafi <109205037+Narimaaaan@users.noreply.github.com>
2023-04-05 22:57:31 +0330

    Delete E directory

--- E/2.js
+++
@@ -1,9 +0,0 @@
-function separator(str) {
-  const splitedOutput = str.split("");
-  return `
-  saal: ${splitedOutput.slice(0, 2).join("")}
-  maah: ${splitedOutput.slice(2, 4).join("")}
-  rooz: ${splitedOutput.slice(4, 6).join("")}
-  `;
-}
-console.log(separator("000608"));
